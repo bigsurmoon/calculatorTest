f27a0ed5422ae851a28e3aea79fe249576301fd0
bigsurmoon <vinny4000@gmail.com>
2024-06-26 18:14:42 +0700

    deleted values

--- src/test/java/ru/se/calculator/helper/TestValues.java
+++ src/test/java/ru/se/calculator/helper/TestValues.java
@@ -5,5 +5,4 @@ public class TestValues {
     public static final String BASE_URL = "https://testsheepnz.github.io/BasicCalculator";
     public static final String FIRST_NUMBER = "2";
     public static final String SECOND_NUMBER = "3";
-    public static final String ANSWER = "3";
 }
